e2a465900e9d5e55040fcca85a128cb979e2e46e
amanuel317 <125757757+amanuel317@users.noreply.github.com>
2024-09-02 20:12:42 +0300

    3. Implement Role-Based Access Control in Django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,6 +4,10 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
 
 # Custom registration view
 def register(request):
@@ -23,3 +27,26 @@ class CustomLoginView(LoginView):
 # Custom logout view
 class CustomLogoutView(LogoutView):
     template_name = 'relationship_app/logout.html'
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
